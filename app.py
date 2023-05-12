230cc4c44444426a1d21a2e7e9576cdb6c3d43a0
YOO JIWAN <outgrowthr@gmail.com>
2023-05-12 17:41:01 +0900

    최종_remin(ver.1)

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ ca = certifi.where()
 client = MongoClient('mongodb+srv://sparta:test@cluster0.isqkcv6.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/re_min', methods=["POST"])
-def re_min_post():
+@app.route("/reviews", methods=["POST"])
+def reviews_post():
     name_receive = request.form['name_give']
     star_receive = request.form['star_give']
     comment_receive = request.form['comment_give']
     img_receive = request.form['img_give']
     pw_receive = request.form['pw_give']
-
 
     doc = {
         'name' : name_receive,
@@ -30,15 +30,15 @@ def re_min_post():
         'pw' : pw_receive
     }
 
-    db.re_min.insert_one(doc)
-    print("1번")
-    return jsonify({'msg':'작성완료'})
+    db.reviews.insert_one(doc)
 
-@app.route('/re_min', methods=["GET"])
-def re_min_get():
-    all_re_min = list(db.re_min.find({},{'_id':False}))
+    return jsonify({'msg':'게시글 작성 완료!'})
 
-    return jsonify({'result' : all_re_min})
+@app.route("/reviews", methods=["GET"])
+def reviews_get():
+    all_reviews = list(db.reviews.find({},{'_id':False}))
+
+    return jsonify({'result':all_reviews})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/index.js
+++ static/index.js
@@ -1,103 +1,132 @@
-$(function () {
-    listing()
+$(function() {
+    $(document).ready(function () {
+        listing();
+    });
 
-function listing() {
-    fetch('/re_min').then((res) => res.json()).then((data) => {
-        let rows = data['result']
-        $('#columns').empty()
-        // console.log(rows)
-        rows.forEach((a) => {
-            console.log(a)
-            let img = a['img']
-            let name = a['name']
-            let star = a['star']
-            let comment = a['comment']
+    function listing() {
+        fetch('/reviews').then((res) => res.json()).then((data) => {
+            let rows = data['result'];
+            $('#columns').empty();
 
-            let star_repeat = '⭐'.repeat(star)
+            rows.forEach((a) => {
+                let image = a['img'];
+                let name = a['name'];
+                let star = a['star'];
+                let comment = a['comment'];
+    
+                let star_repeat = '⭐'.repeat(star);
 
-            let temp_html = `<figure>
-                                    <img src="${img}"/>
-                                    <figcaption class="name">
-                                         ${name}
-                                     </figcaption>
-                   
-                                     <figcaption class="star">
-                                         ${star_repeat}
-                                     </figcaption>
-                                     <figcaption class="caption">
-                                          ${comment}
-                                      </figcaption>
-                                 </figure>`
-            $('#columns').prepend(temp_html)
+                let temp_html = `<figure class="content">
+                                    <img src="${image}">
+                                    <h3 class="name">${name}</h3>
+                                    <p class="star">${star_repeat}</p>
+                                    <figcaption class="caption">${comment}</figcaption>
+                                </figure>`;
+                $('#columns').prepend(temp_html);
+            });
+
+            //  게시글 모달
+            $('.content').click(function() {
+                let info_modal = $('.info-overlay');
+
+                let $image = $(this).children('img').attr('src');
+                let $name = $(this).children('h3').text();
+                let $star = $(this).children('p').text();
+                let $comment = $(this).children('figcaption').text();
+
+                $('.info-image img').attr('src',`${$image}`);
+                $('.info-name').text($name);
+                $('.info-star').text($star);
+                $('.info-comment').text($comment);
+
+                info_modal.css({
+                    'display' : 'block'
+                });
+            });
         });
-    })
+    }
 
-}
+    function save_remin() {
+        let name = $('#aname').val();
+        let star = $('#star').val();
+        let comment = $('#comment').val();
+        let img = $('#img').val();
+        let pw = $('#pw').val();
 
-function save_remin() {
-    let name = $('#aname').val()
-    let star = $('#star').val()
-    let comment = $('#comment').val()
-    let img = $('#img').val()
-    let pw = $('#pw').val()
+        let formData = new FormData();
+        formData.append("name_give", name);
+        formData.append("star_give", star);
+        formData.append("comment_give", comment);
+        formData.append("img_give", img);
+        formData.append("pw_give", pw);
 
+        fetch('/reviews', { method: "POST", body: formData }).then((res) => res.json()).then((data) => {
+            alert(data['msg'])
+            window.location.reload()
+        })
+    }
 
-    let formData = new FormData();
-    formData.append("name_give", name);
-    formData.append("star_give", star);
-    formData.append("comment_give", comment);
-    formData.append("img_give", img);
-    formData.append("pw_give", pw);
-
-    fetch('/re_min', { method: "POST", body: formData }).then((res) => res.json()).then((data) => {
-        alert(data['msg'])
-        window.location.reload()
-    })
-}
-
-// share Modal
-let share_modal = $('.share-overlay');
-
-$('.share').click(function () {
-    share_modal.css({
-        'display': 'block',
+    
+    //  게시글 모달 영역 밖 클릭 시 모달창 닫기
+    $(document).click(function(e) {
+        if( $('#info').is(e.target) ) {
+            $('.info-overlay').css({
+                'display' : 'none'
+            });
+        }
     });
-});
 
-$('.cancel').click(function () {
-    share_modal.css({
-        'display': 'none'
+    //  공유하기 모달
+
+    $('.share').click(function() {
+        let share_modal = $('.share-overlay');
+
+        share_modal.css({
+            'display' : 'block'
+        });
     });
-});
 
-$('.submit').click(save_remin)
-
-// info Modal
-$('#columns figure').click(() => {
-    let info_modal = $('.info-overlay');
-    let click_dataNum = $(this).data('num');
-    fetch('/reviews').then((res) => res.json()).then((data) => {
-        let info = data['result'];
-        let image = info[click_dataNum]['image'];
-        let name = info[click_dataNum]['name'];
-        let star = info[click_dataNum]['star'];
-        let comment = info[click_dataNum]['comment'];
-        let star_repeat = ':별:'.repeat(star);
-        let temp_html = `
-                        <div class="info-image">
-                            <img src="${image}" alt="${name}">
-                        </div>
-                        <div class="info-text">
-                            <p class="info-name">${name}</p>
-                            <p class="info-star">${star_repeat}</p>
-                            <p class="info-comment">${comment}</p>
-                        </div>
-                        `;
-        $('.info-flex').html(temp_html);
+    $('.cancel').click(function() {
+        let share_modal = $('.share-overlay');
+        
+        share_modal.css({
+            'display' : 'none'
+        });
     });
-    info_modal.css({
-        'display' : 'block'
-    });
-});
 
+    $('.submit').click(check);
+
+    //  공유하기 유효성 검사
+
+    function check() {
+        const checkName = $('#overlay #aname');
+        const checkStar = $('#overlay #star');
+        const checkComment = $('#overlay #comment');
+        const checkImg = $('#overlay #img');
+        const checkPw = $('#overlay #pw');
+
+        if ( !checkName.val() ) {      //  frn의 product의 value값이 없을 때 = input에 입력한 값이 없을 때
+            alert('상호명을 입력해 주세요');
+            checkName.focus();
+            return false;   //  경고창을 확인한 후 페이지가 넘어가지 않고 그대로 유지하기 위함, method빼면 못넘어감.
+        } else if ( checkStar.val() == 0 ) {
+            alert('별점을 선택해 주세요');
+            checkStar.focus();
+            return false;
+        } else if ( !checkComment.val() ) {
+            alert('코멘트를 입력해 주세요');
+            checkComment.focus();
+            return false;
+        } else if ( !checkImg.val() ) {
+            alert('이미지 URL을 입력해 주세요');
+            checkImg.focus();
+            return false;
+        } else if ( !checkPw.val() ) {
+            alert('비밀번호 입력해 주세요');
+            checkPw.focus();
+            return false;
+        } else {
+            save_remin();
+        }
+    }
 })
